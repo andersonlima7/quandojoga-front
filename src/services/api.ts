89e0fcc7afc9c182690b5c73410f5667127114e3
andersonlima7 <andersonlll007@gmail.com>
2023-05-22 23:47:41 -0300

    add env api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
+const apiUrl = import.meta.env.VITE_API_URL;
 
-const apiUrl = 'http://localhost:3333/';
+console.log(apiUrl);
 
 export const api = axios.create({
   baseURL: apiUrl,
